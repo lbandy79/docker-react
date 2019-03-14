8b022f9f951c45ec0e5db9a4b2342a8985d0749b
603022 <lukebandy@c02xn085jgh7.users.bah.com>
2019-03-14 08:33:36 -0400

    Added Color Picker React Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Button } from './Button.js';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { color: [1, 46, 89] };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      color: this.chooseColor()
+    });
+  }
+
+  componentDidMount() {
+    this.applyColor();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this.applyColor();
+  }
+
+  formatColor(ary) {
+    return 'rgb(' + ary.join(', ') + ')';
+  }
+
+  isLight() {
+    const rgb = this.state.color;
+    return rgb.reduce((a, b) => a + b) < 127 * 3;
+  }
+
+  applyColor() {
+    const color = this.formatColor(this.state.color);
+    document.body.style.background = color;
+  }
+
+  chooseColor() {
+    const random = [];
+    for (let i = 0; i < 3; i++) {
+      random.push(Math.floor(Math.random() * 256));
+    }
+    return random;
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Put Project Here!!!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1 className={this.isLight() ? 'white' : 'black'}>Your color is {this.formatColor(this.state.color)}!</h1>
+        <Button
+          light={this.isLight()}
+          onClick={this.handleClick} />
       </div>
     );
   }
